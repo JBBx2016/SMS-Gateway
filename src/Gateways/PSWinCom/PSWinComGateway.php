dfadbd904fc39055fef4a8e2e4804d0e24dcd6ce
Johannes Bruvik <jbb@netpower.no>
2018-04-05 15:28:44 +0200

    v1.4: Add PSWinCom support

---
+++ src/Gateways/PSWinCom/PSWinComGateway.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Johannes Bruvik
+ * Date: 05.04.2018
+ * Time: 15:20
+ */
+
+namespace JBBx2016\SMSGateway\Gateways\PSWinCom;
+
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use JBBx2016\SMSGateway\Common\Exceptions\GatewayEndpointConnectionFailedException;
+use JBBx2016\SMSGateway\Common\Exceptions\OnlySMSPayloadAllowedException;
+use JBBx2016\SMSGateway\Common\Gateway\Gateway;
+use JBBx2016\SMSGateway\Common\Payload;
+use JBBx2016\SMSGateway\Common\PhoneNumber;
+use JBBx2016\SMSGateway\Common\Sender;
+use JBBx2016\SMSGateway\Payloads\SMSPayload;
+use libphonenumber\NumberParseException;
+use libphonenumber\PhoneNumberUtil;
+
+class PSWinComGateway extends Gateway
+{
+    /** @var string */
+    private $username;
+
+    /** @var string */
+    private $password;
+
+    /**
+     * PSWinComGateway constructor.
+     * @param string $username
+     * @param string $password
+     */
+    public function __construct($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    /**
+     * @param PhoneNumber $PhoneNumber
+     * @return bool
+     */
+    public function CanProcessPhoneNumber(PhoneNumber $PhoneNumber)
+    {
+        try {
+            return PhoneNumberUtil::getInstance()->isValidNumber($PhoneNumber->toLibPhoneNumber());
+        } catch (NumberParseException $e) {
+            return false;
+        }
+    }
+
+    /**
+     * @param Sender $Sender
+     * @param Payload $Payload
+     * @return void
+     * @throws OnlySMSPayloadAllowedException
+     * @throws GatewayEndpointConnectionFailedException
+     */
+    public function SendMessage(Sender $Sender, Payload $Payload)
+    {
+        if (!($Payload instanceof SMSPayload))
+            throw new OnlySMSPayloadAllowedException($Payload);
+
+        $guzzleClient = new Client([
+            'base_uri' => 'https://simple.pswin.com',
+        ]);
+
+        try {
+            $response = $guzzleClient->request(
+                'GET',
+                '',
+                [
+                    'USER' => $this->username,
+                    'PW' => $this->password,
+                    'RCV' => $Payload->GetPhoneNumber()->getCountryCode() . $Payload->GetPhoneNumber()->getPhoneNumber(),
+                    'SND' => $Sender->GetString(),
+                    'TXT' => $Payload->GetText(),
+                ]
+            );
+        } catch (GuzzleException $e) {
+            error_log($e);
+            throw new GatewayEndpointConnectionFailedException();
+        }
+
+        if ($response->getStatusCode() !== 200) {
+            throw new GatewayEndpointConnectionFailedException($response->getBody());
+        }
+    }
+}
